d5f26ab67d0beb194d7712929ae5d65864f54863
NeverFade <chuanlukk@163.com>
2025-01-05 22:25:48 +0800

    update config

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 NAME="--name exp-name"
 CLASSES="--num_classes 8"
-MODEL="--model_path path/to/your/model_epoch_best.pth"
+MODEL="--model_path path/to/your/model_epoch_best_or_latest.pth"
 DATAROOT="--dataroot path/to/your/DMADataset"
 python eval.py $DATAROOT $NAME $CLASSES $MODEL > ./eval-result-record.txt
